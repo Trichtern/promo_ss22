34d711b155ddea8566560116da1d1ddc1f65b527
Thomas Meaney <thomas.meaney@icloud.com>
2022-05-31 23:00:53 +0200

    Update aufgabe3_2.hs

--- u03/aufgabe3_2.hs
+++ u03/aufgabe3_2.hs
@@ -5,16 +5,14 @@ main :: IO ()
 main = do
     putStrLn "Deutsches Wort:"
     deutsch <- getLine
-    if null deutsch -- beendet die Schleife
+    putStrLn "Bairisches Wort:"
+    bairisch <- getLine
+
+    if null deutsch || null bairisch -- beendet die Schleife
         then return ()
         else do
-            putStrLn "Bairisches Wort:"
-            bairisch <- getLine
-            if null bairisch -- beendet die Schleife
-                then return ()
-                else do
-                    putStrLn ("'" ++ deutsch ++ "' heisst auf Bairisch '" ++ bairisch ++ "'")
-                    schreibGriff <- openFile "woerterbuch.txt" AppendMode
-                    hPutStr schreibGriff (deutsch ++ " " ++ bairisch ++ "\n")
-                    hClose schreibGriff
-                    main -- rekursiver Aufruf fuer die Schleife
+            putStrLn ("'" ++ deutsch ++ "' heisst auf Bairisch '" ++ bairisch ++ "'")
+            schreibGriff <- openFile "woerterbuch.txt" AppendMode
+            hPutStr schreibGriff (deutsch ++ " " ++ bairisch ++ "\n")
+            hClose schreibGriff
+            main -- rekursiver Aufruf fuer die Schleife
